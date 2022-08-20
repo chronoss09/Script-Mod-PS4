b217fe0d5068dbb5f72e5e2f6d7ff2eee49b2755
Chronoss <chronoss0109@gmail.com>
2022-08-20 05:31:07 +0200

    Add files via upload

---
+++ Extra File PSX Game/Warpath Jurassic Park/SLUS00976.lua
@@ -0,0 +1,8 @@
+-- Warpath Jurassic Park
+-- ported to PS4 lua
+-- emu used=syphonfilter v2
+local patcher = function()
+--Widescreen
+R3K_WriteMem16(0x80041558,0x0C00)
+end
+EM_AddVsyncHook(patcher)
